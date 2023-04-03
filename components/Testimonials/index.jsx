8a387266bb1c57086c2d638d7b3c64e2d9063756
AK-Endel <taheener@gmail.com>
2023-04-03 17:41:19 +0300

    fixed testimonals

--- components/Testimonials/index.jsx
+++ components/Testimonials/index.jsx
@@ -4,31 +4,30 @@ import SingleTestimonial from "./SingleTestimonial";
 const testimonialData = [
   {
     id: 1,
-    name: "Suvaline Ment",
-    designation: "Täiskohaga kukk",
-    content:
-      "Ei tümakas on norm.",
-    image: "/images/testimonials/ment.png",
-    star: 5,
-  },
-  {
+    name: "Sarah Johnson",
+    designation: "Music Blogger",
+    content: "As a music blogger, I come across a lot of new artists and labels, but this one really stood out to me. Their artist has a unique voice that draws you in and the production is top-notch. I can see this label becoming a major player in the industry, and I'm excited to see what they come up with next.",
+    image: "/images/testimonials/sarah-johnson.png",
+    star: 4
+    },
+    
+    {
     id: 2,
-    name: "Malle Maalt",
-    designation: "Kaevur",
-    content:
-      "Veits kuulan ikka ja.",
-    image: "/images/testimonials/neiu.png",
-    star: 4,
-  },
-  {
+    name: "Michael Lee",
+    designation: "Music Producer",
+    content: "I've had the pleasure of working with this label's artist, and I have to say, they're a true talent. The label has done an excellent job of promoting and supporting their artist, and it shows in the quality of their music. I highly recommend this label to anyone looking for fresh and exciting new talent.",
+    image: "/images/testimonials/michael-lee.png",
+    star: 5
+    },
+    
+    {
     id: 3,
-    name: "Hood Gangster",
-    designation: "Töötu",
-    content:
-      "Täielik saast pange kinni.",
-    image: "/images/testimonials/gangster.png",
-    star: 1,
-  },
+    name: "Andy Ford",
+    designation: "Music Critic",
+    content: "I've been following this label's artist for a while now, and I have to say, they just keep getting better and better. The label has done an excellent job of cultivating their artist's unique sound. I highly recommend giving this label a listen if you're looking for something fresh and innovative.",
+    image: "/images/testimonials/andy-ford.png",
+    star: 4
+    }
 ];
 
 const Testimonials = () => {
